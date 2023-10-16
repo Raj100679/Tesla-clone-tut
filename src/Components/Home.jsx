9f0121efceda0cb0394ec9d42b7656a4a915a346
Raj <rajkasat@yo.com>
2023-10-17 01:18:22 +0530

    Added Home page and styled it

---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+  return (
+    <Container>
+        <Section/>
+    </Container>
+  )
+}
+
+const Container=styled.div`
+   font-size:20px;
+`
+export default Home
