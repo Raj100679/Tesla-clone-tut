bd8ba0a1a0648dca5b69c0e40d0f13701a696abf
Raj <rajkasat@yo.com>
2023-10-18 01:53:47 +0530

    Made the header section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
-}
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        <a href="#">Model-S</a>
 
-export default Header
+        <a href="#">Model-3</a>
+
+        <a href="#">Model-X</a>
+
+        <a href="#">Model-Y</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+      </RightMenu>
+    </Container>
+  );
+}
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  width: 100vw;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+  }
+`;
+const RightMenu = styled.div`
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right:10px;
+  }
+`;
+
+export default Header;
